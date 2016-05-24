b86df1e82155da6734fc4641b92df5f46f015f10
Patricia <patricia.olivia.link@gmail.com>
2016-05-24 17:51:03 +0200

    Registrierung, Login, Logout und Routes

--- cono_app/www/js/app.js
+++ cono_app/www/js/app.js
@@ -4,70 +4,94 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'firebase'])
+var firebaseUrl = "https://cono-app.firebaseio.com/";
+var starter = angular.module('starter', ['ionic', 'firebase'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+        .factory("Auth", ["$firebaseAuth", "$rootScope",
+            function ($firebaseAuth, $rootScope) {
+                
+                var ref = new Firebase(firebaseUrl);
+                return $firebaseAuth(ref);
+            }])
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+        .run(function ($ionicPlatform, $rootScope, Auth) {
+            $ionicPlatform.ready(function () {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                    cordova.plugins.Keyboard.disableScroll(true);
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+                }
+                if (window.StatusBar) {
+                    // org.apache.cordova.statusbar required
+                    StatusBar.styleDefault();
+                }
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+            });
+             //stateChange event
+             $rootScope.auth = Auth;
+             $rootScope.auth.$onAuth(function(authData, toState) {
+             if (authData) {
+             console.log("Logged in as:", authData.uid);
+             } else {
+             console.log("Logged out");
+             }
+             });
+        })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider               
+                    .state('login', {
+                        url: '/',
+                        templateUrl: 'templates/login.html'
+                    })
+                    
+                    .state('app', {
+                        resolve: {
+                            // controller will not be loaded until $requireAuth resolves
+                            // Auth refers to our $firebaseAuth wrapper in the example above
+                            "currentAuth": ["Auth", function (Auth) {
+                                    // $requireAuth returns a promise so the resolve waits for it to complete
+                                    // If the promise is rejected, it will throw a $stateChangeError (see above)
+                                    return Auth.$requireAuth();
+                                }]
+                        },
+                        url: '/app',
+                        abstract: true,
+                        templateUrl: 'templates/menu.html',
+                        controller: 'SessionCtrl'
+                    })
+                    
+                    .state('app.index', {
+                        
+                        url: '/index',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'index.html'
+                            }
+                        }
+                    })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+                    .state('app.search', {
+                        
+                        url: '/search',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/search.html'
+                            }
+                        }
+                    })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+                    .state('app.browse', {
+                        url: '/browse',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/browse.html'
+                            }
+                        }
+                    });
+                                 
+            // if none of the above states are matched, use this as the fallback
+            $urlRouterProvider.otherwise('/');
+        });

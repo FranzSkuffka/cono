219c1829bb8791ceeda98ad07b8e91880256ffa4
Mirjam Bornschein <m.bornschein@gmail.com>
2016-05-25 17:50:03 +0200

    Work in progress: refinement of conference CRUD

--- cono_web/assets/js/controllers/dashboard.js
+++ cono_web/assets/js/controllers/dashboard.js
@@ -1,14 +1,15 @@
 (function() {
-app.controller("dashboardController", function($scope, $firebaseArray) {
+app.controller("dashboardController", function($scope, $firebaseArray, $location) {
 
   $scope.conferences = $firebaseArray(fbRef.child('conferences'));
   $scope.user = { // TODO: implement Auth
     ID: 'someUserId'
   };
 
-  $scope.removeConference = function(conference) {
-    return $scope.conferences.$remove(conference);
-  };
+  $scope.changeView = function(conference){
+    var conferencePath = '/edit/' + conference.name;
+    $location.path(conferencePath);
+  }
 
   return $scope;
 });

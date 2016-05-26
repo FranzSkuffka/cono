ff942e395a084fc0f505c1b1b1ab04459e40ca5f
Jan Wirth <jottenwe@gmail.com>
2016-05-26 15:50:04 +0200

    Implement auth

--- cono_web/assets/js/main.js
+++ cono_web/assets/js/main.js
@@ -1,10 +1,19 @@
 (function() {
   window.app = angular.module("cono", ["firebase", "ui.router"]);
+  app.constant('loginRedirectPath', '/login');
   window.fbRef = new Firebase("cono-app.firebaseio.com");
+  app.factory("Auth", ["$firebaseAuth", function($firebaseAuth) {
+    return $firebaseAuth(fbRef);
+  }]);
 
   app.config(function($stateProvider, $urlRouterProvider) {
 
 
+    var requireAuth = {
+      "currentAuth": ["Auth", function(Auth) {
+        return Auth.$requireAuth();
+      }]
+    };
 
     $urlRouterProvider.otherwise('/');
 
@@ -14,14 +23,16 @@
       .state('dashboard', {
           url: '/',
           templateUrl: 'templates/dashboard.html',
-          controller: 'dashboardController'
+          controller: 'dashboardController',
+          resolve: requireAuth
       })
 
       // CREATE + EDIT + DELETE
       .state('edit', {
           url: '/edit',
           templateUrl: 'templates/edit.html',
-          controller: 'editConferenceController'
+          controller: 'editConferenceController',
+          resolve: requireAuth
       })
 
       // LOGIN & REGISTRATION
@@ -35,21 +46,24 @@
       .state('settings', {
           url: '/settings',
           templateUrl: 'templates/settings.html',
-          controller: 'settingsController'
+          controller: 'settingsController',
+          resolve: requireAuth
       })
 
       // TALK
       .state('talk', {
           url: '/talk',
           templateUrl: 'templates/talk.html',
-          controller: 'talkController'
+          controller: 'talkController',
+          resolve: requireAuth
       })
 
       // TRACK
       .state('track', {
           url: '/track',
           templateUrl: 'templates/track.html',
-          controller: 'trackController'
+          controller: 'trackController',
+          resolve: requireAuth
       })
 
   });

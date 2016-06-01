48a0ea5e15d632fd31dcd7647841e22ee462268d
Jan Wirth <jottenwe@gmail.com>
2016-06-01 16:12:23 +0200

    Hide button on home screen

--- assets/js/controllers/user.js
+++ assets/js/controllers/user.js
@@ -3,7 +3,7 @@
 
     $rootScope.$on('$stateChangeStart', 
     function(event, toState, toParams, fromState, fromParams){
-        if (toState.name == 'edit' || 'new')
+        if (toState.name == 'edit' || toState.name == 'new')
           $scope.canGoHome = true;
         else
           $scope.canGoHome = false;
